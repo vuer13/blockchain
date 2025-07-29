83097c6b038b3e7758b72ceec14c0c9e64ce7632
Ryan Vu <ryanvu1131@gmail.com>
2025-07-28 19:13:42 -0700

    block made

---
+++ main.js
@@ -0,0 +1,23 @@
+const SHA256 = require('crypto-js/sha256')
+
+class Block {
+    constructor(index, timestamp, data, previousHash = '') {
+        // index: where the block sits on the chain
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+    }
+
+    calculateHash() {
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString()
+    }
+}
+
+class Blockchain {
+    constructor() {
+
+    }
+    
+}

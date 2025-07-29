8bc12369101695a7a9f1580e95e087ebfb0b74a4
Ryan Vu <ryanvu1131@gmail.com>
2025-07-28 19:23:53 -0700

    chain implementation

--- main.js
+++ main.js
@@ -17,7 +17,39 @@ class Block {
 
 class Blockchain {
     constructor() {
-
+        this.chain = [this.createGenesisBlock()];
     }
-    
+
+    // First Block in the chain, returns a new block
+    createGenesisBlock() {
+        return new Block(0, '01/01/2025', 'Gensis Block', '0');
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+
+    // Verfies integrity of the chain
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const curr = this.chain[i];
+            const prev = this.chain[i - 1];
+
+            if (curr.hash !== curr.calculateHash()) {
+                return false
+            }
+
+            if (curr.previousHash !== prev.hash) {
+                return false
+            }
+        }
+        return true
+    }
+
 }

85802a32e0eaa72d3a1408571426980c4b938141
Ryan Vu <ryanvu1131@gmail.com>
2025-07-29 23:37:24 -0700

    mining

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 const SHA256 = require('crypto-js/sha256')
 
+class Transactions {
+    constructor(fromAddress, toAddress, amount) {
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
+    }
+}
+
 class Block {
     constructor(timestamp, transcations, previousHash = '') {
         this.timestamp = timestamp;
@@ -26,6 +34,8 @@ class Blockchain {
     constructor() {
         this.chain = [this.createGenesisBlock()];
         this.difficulty = 2;
+        this.pendingTransactions = [];
+        this.miningReward = 100;
     }
 
     // First Block in the chain, returns a new block
@@ -37,10 +47,45 @@ class Blockchain {
         return this.chain[this.chain.length - 1];
     }
 
+    // NOT NEEDED DUE TO FUNCTION BELOW
     addBlock(newBlock) {
         newBlock.previousHash = this.getLatestBlock().hash;
         newBlock.mineBlock(difficulty);
         this.chain.push(newBlock);
+    }
+
+    minePendingTransactions(miningRewardAddress) {
+        let block = new Block(Data.now(), this.pendingTransactions);
+        block.mineBlock(this.difficulty);
+
+        console.log("Block sucessfully mined");
+        this.chain.push(block);
+
+        this.pendingTransactions = [
+            new Transactions(null, miningRewardAddress, this.miningReward)
+        ];
+    }
+
+    createTransaction(transaction) {
+        this.pendingTransactions.push(transaction);
+    }
+
+    getBalanceofAddress(address) {
+        let balance = 0;
+
+        for(const block of this.chain) {
+            for (const trans of block.transcations) {
+                if (trans.fromAddress === address) {
+                    balance -= trans.amount;
+                }
+
+                if(trans.toAddress === address) {
+                    balance += trans.amount;
+                }
+            }
+        }
+
+        return balance;
     }
 
     // Verfies integrity of the chain
@@ -50,13 +95,13 @@ class Blockchain {
             const prev = this.chain[i - 1];
 
             if (curr.hash !== curr.calculateHash()) {
-                return false
+                return false;
             }
 
             if (curr.previousHash !== prev.hash) {
-                return false
+                return false;
             }
         }
-        return true
+        return true;
     }
 }

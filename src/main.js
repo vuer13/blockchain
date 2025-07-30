177c59f1bd6d697b297733461678a0658cc47754
Ryan Vu <ryanvu1131@gmail.com>
2025-07-30 00:45:35 -0700

    spacing change

--- src/main.js
+++ src/main.js
@@ -7,9 +7,9 @@ rvuCoin.createTransaction(new Transactions('address 2', 'address 1', 20));
 console.log('\n Mining: ')
 rvuCoin.minePendingTransactions('ryans-address')
 
-console.log('\n Balance is', rvuCoin.getBalanceofAddress('ryans-address'))
+console.log('\n Balance is: ', rvuCoin.getBalanceofAddress('ryans-address'))
 
 console.log('\n Mining: ')
 rvuCoin.minePendingTransactions('ryans-address')
 
-console.log('\n Balance is', rvuCoin.getBalanceofAddress('ryans-address'))
+console.log('\n Balance is: ', rvuCoin.getBalanceofAddress('ryans-address'))
